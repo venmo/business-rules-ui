eaaa7cfc15dc9891f9f1cb598ce5c8a82d230190
Greg Einfrank <greg.einfrank@venmo.com>
2014-04-04 00:47:01 -0400

    Clean up demo a bit for actions

--- examples/js/demo.js
+++ examples/js/demo.js
@@ -61,7 +61,7 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
     allData = getInitialData();
 
     initializeConditions(allData);
-    initializeActions();
+    initializeActions(allData);
     initializeForm();
   }
 
@@ -69,62 +69,8 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
     conditions.conditionsBuilder(data)
   }
 
-  function initializeConditionsOld() {
-    conditions.conditionsBuilder({
-      fields: [
-        {label: "Name", name: "nameField", operators: [
-          {label: "is present", name: "present", fieldType: "none"},
-          {label: "is blank", name: "blank", fieldType: "none"},
-          {label: "is equal to", name: "equalTo", fieldType: "text"},
-          {label: "is not equal to", name: "notEqualTo", fieldType: "text"},
-          {label: "includes", name: "includes", fieldType: "text"},
-          {label: "matches regex", name: "matchesRegex", fieldType: "text"}
-        ]},
-        {label: "Age", name: "ageField", operators: [
-          {label: "is present", name: "present", fieldType: "none"},
-          {label: "is blank", name: "blank", fieldType: "none"},
-          {label: "is equal to", name: "equalTo", fieldType: "text"},
-          {label: "is not equal to", name: "notEqualTo", fieldType: "text"},
-          {label: "is greater than", name: "greaterThan", fieldType: "text"},
-          {label: "is greater than or equal to", name: "greaterThanEqual", fieldType: "text"},
-          {label: "is less than", name: "lessThan", fieldType: "text"},
-          {label: "is less than or equal to", name: "lessThanEqual", fieldType: "text"},
-        ]},
-        {label: "Occupation", name: "occupationField", options: occupationOptions, operators: [
-          {label: "is present", name: "present", fieldType: "none"},
-          {label: "is blank", name: "blank", fieldType: "none"},
-          {label: "is equal to", name: "equalTo", fieldType: "select"},
-          {label: "is not equal to", name: "notEqualTo", fieldType: "select"},
-        ]}
-      ],
-      data: {"all": [
-        {name: "nameField", operator: "equalTo", value: "Godzilla"},
-        {name: "ageField", operator: "greaterThanEqual", value: "21"}
-      ]}
-    });
-  }
-
-  function initializeActions() {
-    actions.actionsBuilder({
-      actions: [
-        { name: "put_on_sale",
-          label: "Put On Sale",
-          params: {sale_percentage: "numeric"}},
-        { name: "order_more",
-          label: "Order More",
-          params: {number_to_order: "numeric"}}
-      ],
-      data: [
-        {name: "action-select", value: "alert", fields: [
-          {name: "message", value: "Hello world!"}
-        ]},
-        {name: "action-select", value: "updateField", fields: [
-          {name: "fieldId", value: "occupationField", fields: [
-            {name: "newValue", value: "marketing"}
-          ]}
-        ]}
-      ]
-    });
+  function initializeActions(data) {
+    actions.actionsBuilder(data);
   }
 
   function initializeForm() {
