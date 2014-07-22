d509acdc6e7bc989964892226b423a024e7710a3
Greg Einfrank <greg.einfrank@venmo.com>
2014-07-22 13:19:51 -0400

    Fix demo to match new format

--- examples/js/demo.js
+++ examples/js/demo.js
@@ -6,15 +6,15 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
     return {"variables": [
             { "name": "expiration_days",
               "label": "Days until expiration",
-              "variable_type": "numeric",
+              "field_type": "numeric",
               "options": []},
             { "name": "current_month",
               "label": "Current Month",
-              "variable_type": "string",
+              "field_type": "string",
               "options": []},
             { "name": "goes_well_with",
               "label": "Goes Well With",
-              "variable_type": "select",
+              "field_type": "select",
               "options": ["Eggnog", "Cookies", "Beef Jerkey"]}
                         ],
           "actions": [
@@ -28,25 +28,25 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
           "variable_type_operators": {
             "numeric": [ {"name": "equal_to",
                           "label": "Equal To",
-                          "field_type": "numeric"},
+                          "input_type": "numeric"},
                          {"name": "less_than",
                           "label": "Less Than",
-                          "field_type": "numeric"},
+                          "input_type": "numeric"},
                          {"name": "greater_than",
                           "label": "Greater Than",
-                          "field_type": "numeric"}],
+                          "input_type": "numeric"}],
             "string": [ { "name": "equal_to",
                           "label": "Equal To",
-                          "field_type": "text"},
+                          "input_type": "text"},
                         { "name": "non_empty",
                           "label": "Non Empty",
-                          "field_type": "none"}],
+                          "input_type": "none"}],
             "select": [ { "name": "contains",
                           "label": "Contains",
-                          "field_type": "select"},
+                          "input_type": "select"},
                         { "name": "does_not_contain",
                           "label": "Does Not Contain",
-                          "field_type": "select"}]
+                          "input_type": "select"}]
           }
     };
   };
